14009f1b68a6c7aafb414389d9c252619e46684d
davidhjensen <dhjensen02@gmail.com>
2025-03-04 11:58:43 -0700

    first draft on i2c master/slave functions. TODO: fill out header file

---
+++ common/i2c.h
@@ -0,0 +1,15 @@
+// Includes (if needed)
+#include <stdint.h>  // Example: Standard integer types
+
+// Macros (if needed)
+#define COMMON_MACRO 42  // Example macro definition
+
+// Type definitions (if needed)
+typedef struct {
+    int field1;
+    float field2;
+} CommonStruct;
+
+// Function declarations
+void common_function(void);
+int add_numbers(int a, int b);
