14c9c9af92622fbb5530f98e07b292e098247e52
davidhjensen <dhjensen02@gmail.com>
2025-03-08 21:44:03 -0700

    added RGB functionality

--- controller/app/main.c
+++ controller/app/main.c
@@ -1,6 +1,40 @@
 #include <msp430fr2355.h>
 #include <stdbool.h>
 
+// RGB LED colors
+#define RGB_LOCKED 0xFF0000
+#define RGB_UNLOCKING 0x0000FF
+#define RGB_UNLOCKED 0x00FF00
+#define RGB_PAT0 0x808080
+#define RGB_PAT1 0xF0F0F0
+#define RGB_PAT2 0xF0F0F0
+#define RGB_PAT3 0x808080
+#define RGB_PAT4 0xF0F0F0
+#define RGB_PAT5 0x808080
+#define RGB_PAT6 0xF0F0F0
+#define RGB_PAT7 0x808080
+
+// Setup RGB LED
+setupRGBLED();
+int rPWM = 0;
+int gPWM = 0;
+int bPWM = 0;
+int countPWM = 0;
+
+// TODO: setup I2C
+
+// Setup keypad
+setupKeypad();
+char lastKey = 'X';
+
+// STATE
+// 0    Locked
+// 1    First correct digit entered
+// 2    Second
+// 3    Third
+// 4    Unlocked
+int state = 0;
+
 int main(void)
 {
     // Stop watchdog timer
@@ -12,21 +46,6 @@ int main(void)
     // Disable low-power mode / GPIO high-impedance
     PM5CTL0 &= ~LOCKLPM5;
 
-    // TODO: setup RGB LED
-
-    // TODO: setup I2C
-
-    // Setup keypad
-    setupKeypad();
-
-    // STATE
-    // 0    Locked
-    // 1    First correct digit entered
-    // 2    Second
-    // 3    Third
-    // 4    Unlocked
-    int state = 0;
-
     while (true)
     {
         P1OUT ^= BIT0;
@@ -37,7 +56,7 @@ int main(void)
             if (state == 0) {
                 if (key_val=='1') {
                     state = 1;
-                    // TODO: unlocking PWM color
+                    updateHex(RGB_UNLOCKING);
                 } else {
                     state = 0;
                 }
@@ -47,7 +66,7 @@ int main(void)
                     state = 2;
                 } else {
                     state = 0;
-                    // TODO: locked PWM color
+                    updateHex(RGB_LOCKED);
                 }
 
             } else if (state == 2) {
@@ -55,34 +74,52 @@ int main(void)
                     state = 3;
                 } else {
                     state = 0;
-                    // TODO: locked PWM color
+                    updateHex(RGB_LOCKED);
                 }
                 
             } else if (state == 3) {
                 if (key_val=='1') {
                     state = 4;
-                    // TODO: unlocked PWM color
+                    updateHex(RGB_UNLOCKED);
                 } else {
                     state = 0;
-                    // TODO: locked PWM color
+                    updateHex(RGB_LOCKED);
                 }
 
             } else if (state == 4) {
                 // lock
                 if (key_val=='D') { 
                     state = 0;
-                    // TODO: locked PWM color
+                    updateHex(RGB_LOCKED);
                     // TODO: clear LCD command
                     // TODO: clead patterns command
 
-                // anything else
+                // otherwise
                 } else {
+                    else if (key_val=='0') {
+                    updateHex(RGB_PAT0);
+                    } else if (key_val=='1') {
+                        updateHex(RGB_PAT1);
+                    } else if (key_val=='2') {
+                        updateHex(RGB_PAT2);
+                    } else if (key_val=='3') {
+                        updateHex(RGB_PAT3);
+                    } else if (key_val=='4') {
+                        updateHex(RGB_PAT4);
+                    } else if (key_val=='5') {
+                        updateHex(RGB_PAT5);
+                    } else if (key_val=='6') {
+                        updateHex(RGB_PAT6);
+                    } else if (key_val=='7') {
+                        updateHex(RGB_PAT7);
+                    }
                     // TODO: send key press to LCD
                     // TODO: send key press to LED BAR
-                } 
+                }
+                
             } else {
                 state = 0;
-                // TODO: locked PWM color
+                updateHex(RGB_LOCKED);
             }
         }
     }
@@ -91,4 +128,33 @@ int main(void)
 
 // TODO: master I2C ISR
 
-// TODO: RGB LED PWM
+// RGB LED PWM Control
+#pragma vector = TIMER3_B0_VECTOR
+__interrupt void ISR_PWM_PERIOD(void)
+{
+    // update outputs
+    if (countPWM > rPWM) {
+        P1OUT &= ~BIT5;
+    } else {
+        P1OUT |= BIT5;
+    }
+    if (countPWM > gPWM) {
+        P1OUT &= ~BIT6;
+    } else {
+        P1OUT |= BIT6;
+    }
+    if (countPWM > bPWM) {
+        P1OUT &= ~BIT7;
+    } else {
+        P1OUT |= BIT7;
+    }
+    // update count
+    if (countPWM > 255) {
+        countPWM = 0;
+    } else {
+        countPWM += 1;
+    }
+
+    // clear CCR0 IFG
+    TB3CCTL0 &= ~CCIFG;
+}
